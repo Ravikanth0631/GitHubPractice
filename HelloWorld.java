df2914f907242665b8b64bfac0c3843aec32134a
Ravikanth Reddy <ravikanthjalla@gmail.com>
2026-09-10 14:13:30 +0530

    Add calculator functionality

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
+	int a=3;
+	int b=2;
+	int c=a+b;
         System.out.println("Hello GitHub!");
+	System.out.println("sum =" + c);
     }
+
 }
